834284286b21be6df35bd455ca7b1a5fbe11bdef
ozillo <marcmateo25@gmail.com>
2023-11-21 13:28:20 +0100

    Create components Title,Subtitle,Image and Paragraph

---
+++ componentsREACT/components-basics/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+/*
+
+    -Tittle: Componente <h1> con el texto "Welcome to Components ReactJS"
+    -Subtittle: Componente <h2> con el texto "This is a example components with ReactJS"
+    -Image: Crea un componente imagen <img> 
+    -Paragraph: Crea un componente paragraph <p> con el texto que quieras
+
+*/
+
+function App() {
+  return (
+    <>
+  <Title/>
+  <Subtitle/>
+  <Image/>
+  <Paragraph/>
+    </>
+  )
+}
+
+export default App
---
+++ componentsREACT/components-basics/src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Title.css"
+
+const Title = () => {
+  return (
+    <h1>Welcome to Components ReactJS</h1>
+  )
+}
+
+export default Title
